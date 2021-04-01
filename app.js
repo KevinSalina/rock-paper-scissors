470aa590601f56c5a2ae18d95b9d7c93916ca8b2
Kevin Salina <kevin.salina25@gmail.com>
2021-04-01 14:34:51 -0400

    Added main game function, result function, and user input function. Can now play a 5 round game using prompt and console

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
-
-
+// Delcare Game Variables
+let userScore = 0;
+let compScore = 0;
+let round;
 const gameHands = ['r', 'p', 's']
 
-console.log(playRound('scissors', computerHand()))
+// Run Game Function
+game()
 
-// Play 1 round 
+// Main Game Function - 5 Rounds
+function game(){
+    for(round = 1; round <= 5; round++){
+        playRound(userHand(), computerHand())
+    }
+    // After 5 rounds...
+    if (userScore > compScore){
+        console.log(`User Wins! Final Score: ${userScore} - ${compScore}`)
+    } else if ( userScore < compScore){
+        console.log(`User Loses... Final Score: ${userScore} - ${compScore}`)
+    } else {
+        console.log(`It's a draw! Final Score: ${userScore} - ${compScore}`)
+    }
+}
+
+
+// Helper Functions
+// Play a single round 
 function playRound(user, comp){
-    user = user.toLowerCase().charAt(0);
-    console.log(`User: ${user} vs. Comp: ${comp}`)
     // User hand first, computer hand second
     const matchUp = (user + comp);
     switch(matchUp){
         case "rs":
         case "pr":
         case "sp":
-            console.log("User Wins! :)")
+            userScore++
+            roundResults('win', user, comp)
             break;
         case "sr":
         case "rp":
         case "ps":
-            console.log("User Loses.. :(")
+            compScore++
+            roundResults('lose', user, comp)
             break;
         default:
-            console.log(`It's a draw`)    
+            roundResults('draw', user, comp)
     }
 }
 
-// Returns a Random Computer Play
+// Display results for each round
+function roundResults(result, user, comp){
+    if (result === 'win'){
+        console.log(`Round ${round}. ${user} beats ${comp}. User Wins! Current Score - User: ${userScore}, Comp: ${compScore}`)
+    } else if (result === 'lose'){
+        console.log(`Round ${round}. ${user} loses to ${comp}. User loses. Current Score - User: ${userScore}, Comp: ${compScore}`)
+    } else {
+        console.log(`Round ${round}. ${user} ties ${comp}. It's a draw. Current Score - User: ${userScore}, Comp: ${compScore}`)
+    }
+}
+
+// Select a random computer hand
 function computerHand(){
     let randNum = Math.floor(Math.random() * 3)
     return gameHands[randNum]
 }
+
+// Prompts for user Hand
+function userHand(){
+    let userInput = prompt(`Select a hand! (Rock, Paper, Scissors) - Round: ${round}` ).toLowerCase().charAt(0)
+        while(!userInput || !gameHands.includes(userInput)){
+            userInput = prompt(`You must make a valid selection! Rock, Paper, Scissors - Round: ${round}`).toLowerCase().charAt(0)
+        }
+    return userInput
+}
+
 
