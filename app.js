50ae88c38acd7f73ce728313d817d62be5296bf5
Kevin Salina <kevin.salina25@gmail.com>
2021-04-01 13:42:34 -0400

    Added basic game functions - Random computer hand, and play 1 round

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-alert('HELLO')
+
+
+const gameHands = ['r', 'p', 's']
+
+console.log(playRound('scissors', computerHand()))
+
+// Play 1 round 
+function playRound(user, comp){
+    user = user.toLowerCase().charAt(0);
+    console.log(`User: ${user} vs. Comp: ${comp}`)
+    // User hand first, computer hand second
+    const matchUp = (user + comp);
+    switch(matchUp){
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("User Wins! :)")
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            console.log("User Loses.. :(")
+            break;
+        default:
+            console.log(`It's a draw`)    
+    }
+}
+
+// Returns a Random Computer Play
+function computerHand(){
+    let randNum = Math.floor(Math.random() * 3)
+    return gameHands[randNum]
+}
+
