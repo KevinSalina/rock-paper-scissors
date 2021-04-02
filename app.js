e25b7002a41c90fd87a3aba187c226bda1ae3539
Kevin Salina <kevin.salina25@gmail.com>
2021-04-02 17:08:39 -0400

    Changed file names

--- app.js
+++ app.js
@@ -1,75 +1,128 @@
 // Delcare Game Variables
 let userScore = 0;
 let compScore = 0;
-let round;
+let winningScore = 5;
+let gameOver = false;
 const gameHands = ['r', 'p', 's']
 
-// Run Game Function
-game()
+// DOM Elements
+const rock = document.querySelector('#r');
+const paper = document.querySelector('#p');
+const scissors = document.querySelector('#s');
+const gameButtons = [rock, paper, scissors]
+const resetGame = document.querySelector('.reset-game')
 
-// Main Game Function - 5 Rounds
-function game(){
-    for(round = 1; round <= 5; round++){
-        playRound(userHand(), computerHand())
-    }
-    // After 5 rounds...
-    if (userScore > compScore){
-        console.log(`User Wins! Final Score: ${userScore} - ${compScore}`)
-    } else if ( userScore < compScore){
-        console.log(`User Loses... Final Score: ${userScore} - ${compScore}`)
-    } else {
-        console.log(`It's a draw! Final Score: ${userScore} - ${compScore}`)
-    }
+const userScoreDisplay = document.querySelector('.user-score')
+const compScoreDisplay = document.querySelector('.comp-score')
+
+const textDisplay = document.querySelector('.text-display')
+const roundSelectionsDisplay = document.querySelector('.round-selections')
+
+// Main Game Event Listener. If GameOver = false, game will run when user makes a selection.
+gameButtons.forEach(function(choice, index){
+    choice.addEventListener('click', ()=>{
+        switch(index){
+            case 0:
+                game('r', randomComputerChoice())
+                break;
+            case 1:
+                game('p', randomComputerChoice())
+                break;
+            case 2:
+                game('s', randomComputerChoice())
+                break;
+        }
+    })
+})
+
+// Main Game Function
+function game(userChoice, compChoice){
+    if (gameOver) return;
+    playRound(userChoice, compChoice)
 }
 
 
-// Helper Functions
 // Play a single round 
-function playRound(user, comp){
-    // User hand first, computer hand second
-    const matchUp = (user + comp);
+function playRound(userChoice, compChoice){
+    const matchUp = (userChoice + compChoice);
     switch(matchUp){
         case "rs":
         case "pr":
         case "sp":
             userScore++
-            roundResults('win', user, comp)
+            roundResults('win', convertLetterToWord(userChoice), convertLetterToWord(compChoice))
             break;
         case "sr":
         case "rp":
         case "ps":
             compScore++
-            roundResults('lose', user, comp)
+            roundResults('lose', convertLetterToWord(userChoice), convertLetterToWord(compChoice))
             break;
         default:
-            roundResults('draw', user, comp)
+            roundResults('draw', convertLetterToWord(userChoice), convertLetterToWord(compChoice))
     }
 }
 
-// Display results for each round
-function roundResults(result, user, comp){
+// Handle Round Result Displays
+function roundResults(result, userChoice, compChoice){
     if (result === 'win'){
-        console.log(`Round ${round}. ${user} beats ${comp}. User Wins! Current Score - User: ${userScore}, Comp: ${compScore}`)
+        userScoreDisplay.textContent = userScore;
+        roundSelectionsDisplay.textContent = `${userChoice} beats ${compChoice}`
+        if(userScore === winningScore){
+            gameOver = true;
+            userScoreDisplay.classList.add('won')
+            textDisplay.textContent = `Game Over. You Won!`
+            resetGame.classList.add('show')
+        } else {
+        textDisplay.textContent = `You Win!`
+        }
     } else if (result === 'lose'){
-        console.log(`Round ${round}. ${user} loses to ${comp}. User loses. Current Score - User: ${userScore}, Comp: ${compScore}`)
-    } else {
-        console.log(`Round ${round}. ${user} ties ${comp}. It's a draw. Current Score - User: ${userScore}, Comp: ${compScore}`)
+        compScoreDisplay.textContent = compScore;
+        roundSelectionsDisplay.textContent = `${userChoice} loses to ${compChoice}`
+        if(compScore === winningScore){
+            gameOver = true;
+            compScoreDisplay.classList.add('lost')
+            textDisplay.textContent = `Game Over. You lost...`
+            resetGame.classList.add('show')
+        } else {
+        textDisplay.textContent = `You lose`
+        }
+    } else if (result === 'draw'){
+        textDisplay.textContent = `It's a draw.`
+        roundSelectionsDisplay.textContent = `${userChoice} ties ${compChoice}`
     }
 }
+
+// Reset Game Button
+resetGame.addEventListener('click', () =>{
+    gameOver = false;
+    userScore = 0;
+    userScoreDisplay.textContent = userScore;
+    userScoreDisplay.classList.remove('won')
+    compScore = 0;
+    compScoreDisplay.textContent = compScore;
+    compScoreDisplay.classList.remove('lost')
+    textDisplay.textContent = `Choose a hand!`
+    roundSelectionsDisplay.textContent = ''
+    resetGame.classList.remove('show')
+})
+
 
 // Select a random computer hand
-function computerHand(){
+function randomComputerChoice(){
     let randNum = Math.floor(Math.random() * 3)
     return gameHands[randNum]
 }
 
-// Prompts for user Hand
-function userHand(){
-    let userInput = prompt(`Select a hand! (Rock, Paper, Scissors) - Round: ${round}` ).toLowerCase().charAt(0)
-        while(!userInput || !gameHands.includes(userInput)){
-            userInput = prompt(`You must make a valid selection! Rock, Paper, Scissors - Round: ${round}`).toLowerCase().charAt(0)
-        }
-    return userInput
+
+// Helper Functions
+// Convert letter to word for use with DOM display
+function convertLetterToWord(l){
+    if(l === 'r') return 'Rock'
+    if(l === 'p') return 'Paper'
+    if(l === 's') return 'Scissors'
 }
+
+
 
 
